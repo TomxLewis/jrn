0bf6da6ac87a03a9a455671791ca2f1696a7c90d
Tom Lewis <TomxLewis@gmail.com>
2019-09-28 15:56:07 -0400

    Updates code formatting in main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,50 +12,48 @@ fn clap_app<'a, 'b>() -> App<'a, 'b> {
         .setting(AppSettings::VersionlessSubcommands)
         .author("Tom Lewis <tomxlewis@gmail.com")
         .about("Command Line journaling System that Integrates with git for version control.")
-        //DONE
         .subcommand(SubCommand::with_name("list")
-            .about("List jrn entries")
-            .arg(Arg::from_usage("[FILTER] 'List entries where filename contains FILTER'")
-                .default_value(".*")
-                .takes_value(true))
-            .arg(Arg::from_usage("-n [NUM] 'Limit output to most recent NUM of matching entries")))
+                    .about("List jrn entries")
+                    .arg(Arg::from_usage("[FILTER] 'List entries where filename contains FILTER'")
+                        .default_value(".*")
+                        .takes_value(true))
+                    .arg(Arg::from_usage("-n [NUM] 'Limit output to most recent NUM of matching entries")))
         .subcommand(SubCommand::with_name("new")
-            .about("Create a new jrn entry")
-            .arg(Arg::from_usage("[TAGS] 'Tags in the new entry'")
-                .required(false)
-                .takes_value(true)
-                .multiple(true))
-            .arg(Arg::from_usage("-q --quick 'Don't open editor, just create entry'"))
-            .arg(Arg::from_usage("-n --note [TEXT] 'The new entries contents'")
-                .long_help("creates a new entry with TEXT and the default tags provided by the devices config")
-                .takes_value(true)))
+                    .about("Create a new jrn entry")
+                    .arg(Arg::from_usage("[TAGS] 'Tags in the new entry'")
+                        .required(false)
+                        .takes_value(true)
+                        .multiple(true))
+                    .arg(Arg::from_usage("-q --quick 'Don't open editor, just create entry'"))
+                    .arg(Arg::from_usage("-n --note [TEXT] 'The new entries contents'")
+                        .long_help("creates a new entry with TEXT and the default tags provided by the devices config")
+                        .takes_value(true)))
         .subcommand(SubCommand::with_name("log")
                     .about("Logs the most recent entries to stdout"))
         .subcommand(SubCommand::with_name("entries")
-            //TODO implement entries subcommand
-            .arg(Arg::from_usage("[FILTER] 'Operations will apply to entries that match the FILTER'")
-                .long_help("Asks for confirmation on modifying multiple entries, \
-                this behavior can be skipped by passing the -f or --force option")))
-        //
+                    //TODO implement entries subcommand
+                    .arg(Arg::from_usage("[FILTER] 'Operations will apply to entries that match the FILTER'")
+                        .long_help("Asks for confirmation on modifying multiple entries, \
+                        this behavior can be skipped by passing the -f or --force option")))
         .subcommand(SubCommand::with_name("tags")
-            //TODO implement tags subcommand
-            .arg(Arg::from_usage("[FILTER] 'Operations will apply to TAGS that match the filter'")
-                .long_help("Asks for confirmation on modifying multiple entries, \
-                this behavior can be skipped by passing the -f or --force option")))
+                    //TODO implement tags subcommand
+                    .arg(Arg::from_usage("[FILTER] 'Operations will apply to TAGS that match the filter'")
+                        .long_help("Asks for confirmation on modifying multiple entries, \
+                        this behavior can be skipped by passing the -f or --force option")))
         //TODO
         .arg(Arg::from_usage("-c --config [OPTION]=[VALUE]\"\" 'Set a configuration parameter for this run only'")
-            //TODO implement parsing config option=value pairs
-            .long_help("See mod jrn::jrn_lib::config::settings for fields and values")
-            //TODO document all config options
-            .multiple(true)
-            .number_of_values(2))
+                    //TODO implement parsing config option=value pairs
+                    .long_help("See mod jrn::jrn_lib::config::settings for fields and values")
+                    //TODO document all config options
+                    .multiple(true)
+                    .number_of_values(2))
         .subcommand(SubCommand::with_name("config")
-            //TODO implement sub-command "config"
-            .about("Alters or inquires the current jrn configuration")
-            .arg(Arg::with_name("list")
-                .help("Lists all config options and their values")
-                .short("l")
-                .long("list")))
+                    //TODO implement sub-command "config"
+                    .about("Alters or inquires the current jrn configuration")
+                    .arg(Arg::with_name("list")
+                        .help("Lists all config options and their values")
+                        .short("l")
+                        .long("list")))
 }
 
 fn main() {
@@ -98,13 +96,9 @@ fn list(args: &ArgMatches, repo: &mut JrnRepo) {
     repo.list_entries(filter, num).unwrap();
 }
 
-enum LogTime {
-    Today,
-    PastHour,
-}
 /// Display the most recent entries to stdout
 /// shortcut for jrn list -n=5
-fn log(repo: &JrnRepo) { //TODO take a LogTime
+fn log(repo: &JrnRepo) {
     let filter = ".*";
     let num = Some(5);
     repo.list_entries(filter, num).unwrap();
