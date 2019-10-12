6550ee8cfd89d5a0d6a6fac8709da745085883b6
Tom Lewis <TomxLewis@gmail.com>
2019-10-12 19:31:46 -0400

    Flattens package structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
-
 extern crate chrono;
 extern crate dirs;
 extern crate ron;
 extern crate serde;
 extern crate log;
-extern crate simplelog;
 
-mod jrn_lib;
-pub use jrn_lib::*;
+mod error;
+mod time;
+mod config;
+mod repo;
+mod entry;
+mod tag_container;
+
+//internals
+use time::TimeStamp;
+use entry::{JrnEntry, JrnEntryFilter};
+pub use tag_container::{CountAndTag, TagContainer};
+
+//exports
+pub use config::{IgnorePatterns, Settings};
+pub use error::JrnError;
+pub use repo::JrnRepo;
